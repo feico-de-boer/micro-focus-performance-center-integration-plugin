ddb54e0334c97586701ca5e6e36eb16c57fb4e9a
Feico de Boer <feicode.boer@infor.com>
2026-03-13 16:19:14 +0100

    A bit more flexibility and reference to the original jar

--- add-custom-jar.cmd
+++ add-custom-jar.cmd
@@ -1,7 +1,17 @@
+@rem Custom library version
+@rem ----------------------
+LIB=c:/LocalData/performance-center-plugins-common-utils/target/plugins-common-custom-1.2.1.1.jar
+
+@rem Standard library version
+@rem ------------------------
+@rem set LIB=C:/Users/fdboer/Downloads/plugins-common-1.2.0.jar
+@rem set LIB=C:/Users/fdboer/Downloads/plugins-common-1.2.1.jar
+
 "C:/Opt/apache-maven-3.9.9/bin/mvn.cmd" install:install-file ^
-  -Dfile=c:/LocalData/performance-center-plugins-common-utils/target/plugins-common-custom-1.2.1.1.jar ^
+  -Dfile=%LIB% ^
   -DgroupId=com.microfocus.adm.performancecenter ^
   -DartifactId=plugins-common-custom ^
   -Dversion=1.2.1.1 ^
   -Dpackaging=jar
+
 exit
