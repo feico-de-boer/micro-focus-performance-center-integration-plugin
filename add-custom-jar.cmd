7ad1583543d6aafd921a6a36be0d8aa922d9f216
Feico de Boer <feicode.boer@infor.com>
2026-03-13 13:46:46 +0100

    Switch to custom plugins-common library for cloud LG support

---
+++ add-custom-jar.cmd
@@ -0,0 +1,7 @@
+"C:/Opt/apache-maven-3.9.9/bin/mvn.cmd" install:install-file ^
+  -Dfile=c:/LocalData/performance-center-plugins-common-utils/target/plugins-common-custom-1.2.1.1.jar ^
+  -DgroupId=com.microfocus.adm.performancecenter ^
+  -DartifactId=plugins-common-custom ^
+  -Dversion=1.2.1.1 ^
+  -Dpackaging=jar
+exit
